10c6e37f0bf0b23e674e61108f4f131370019ba6
agustinjalil <agustinjalilleon@gmail.com>
2023-01-10 15:40:38 -0300

    todo3

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
+  get 'staff/index'
   root to: "listas#index"
+  resources :staffs
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   # Defines the root path route ("/")
   # root "articles#index"
 end
